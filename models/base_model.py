c61b0b92d4c2ad183b2da5d880f0901ac1b69ee5
mastingo <mativokyle@gmail.com>
2024-07-01 08:57:30 +0300

    1

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return '[__class__.__name__], (self.id), self.__dict__'
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
