5004da1abaa15f45ddc98530a00f465ca5583c66
mastingo <mativokyle@gmail.com>
2024-07-02 11:29:09 +0300

    1-corrections

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+
+"""base model class"""
+
+
 class BaseModel:
+    """init method"""
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    """def str function"""
     def __str__(self):
         return '[__class__.__name__], (self.id), self.__dict__'
 
+    """save function"""
     def save(self):
         self.updated_at = datetime.now()
 
+    """save to dict"""
     def to_dict(self):
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = type(self).__name__
