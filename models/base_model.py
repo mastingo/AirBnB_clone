2f63cfffd5708768377fae204b5040bc049d43f3
mastingo <mativokyle@gmail.com>
2024-07-02 12:33:50 +0300

    2

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
+"""importing modules for the base class"""
+
 import uuid
 from datetime import datetime
 
-"""base model class"""
-
 
 class BaseModel:
+
     """init method"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+    def __init__(self, *args, **kwargs):
+
+        """ class for base model updated for kwargs and args"""
+
+        if kwargs is not None and args != {}:
+
+            if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     """def str function"""
     def __str__(self):
